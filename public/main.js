536ac44df1d67bc74cf5d0c3d637beae26c0aa7c
Robert Johnson <roblockhead>
2019-09-08 20:04:42 -0400

    auth edits

--- public/main.js
+++ public/main.js
@@ -117,7 +117,7 @@ generatePushID = (function() {
   })();
 
 // Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 function epochToJsDate(ts){
   // ts = epoch timestamp
@@ -145,44 +145,56 @@ function signOut() {
 function isUserSignedIn() {
   return !!firebase.auth().currentUser;
 }
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [{
-    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-    // Allow the user the ability to complete sign-in cross device,
-    // including the mobile apps specified in the ActionCodeSettings
-    // object below.
-    forceSameDevice: false,
-    // Used to define the optional firebase.auth.ActionCodeSettings if
-    // additional state needs to be passed along request and whether to open
-    // the link in a mobile app if it is installed.
-    emailLinkSignIn: function() {
-      return {
-        // Additional state showPromo=1234 can be retrieved from URL on
-        // sign-in completion in signInSuccess callback by checking
-        // window.location.href.
-        url: 'https://www.example.com/completeSignIn?showPromo=1234',
-        // Custom FDL domain.
-        dynamicLinkDomain: 'example.page.link',
-        // Always true for email link sign-in.
-        handleCodeInApp: true,
-        // Whether to handle link in iOS app if installed.
-        iOS: {
-          bundleId: 'com.example.ios'
-        },
-        // Whether to handle link in Android app if opened in an Android
-        // device.
-        android: {
-          packageName: 'com.example.android',
-          installApp: true,
-          minimumVersion: '12'
-        }
-      };
-    }
-  },
-    // List of OAuth providers supported.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-});
+// ui.start('#firebaseui-auth-container', {
+//   signInOptions: [{
+//     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+//     // Allow the user the ability to complete sign-in cross device,
+//     // including the mobile apps specified in the ActionCodeSettings
+//     // object below.
+//     forceSameDevice: false,
+//     // Used to define the optional firebase.auth.ActionCodeSettings if
+//     // additional state needs to be passed along request and whether to open
+//     // the link in a mobile app if it is installed.
+//     emailLinkSignIn: function() {
+//       return {
+//         // Additional state showPromo=1234 can be retrieved from URL on
+//         // sign-in completion in signInSuccess callback by checking
+//         // window.location.href.
+//         url: 'https://www.example.com/completeSignIn?showPromo=1234',
+//         // Custom FDL domain.
+//         dynamicLinkDomain: 'example.page.link',
+//         // Always true for email link sign-in.
+//         handleCodeInApp: true,
+//         // Whether to handle link in iOS app if installed.
+//         iOS: {
+//           bundleId: 'com.example.ios'
+//         },
+//         // Whether to handle link in Android app if opened in an Android
+//         // device.
+//         android: {
+//           packageName: 'com.example.android',
+//           installApp: true,
+//           minimumVersion: '12'
+//         }
+//       };
+//     }
+//   },
+//     // List of OAuth providers supported.
+//     firebase.auth.GoogleAuthProvider.PROVIDER_ID
+//   ],
+//   // Other config options...
+// });
+
+function newUser(){
+  email = document.getElementById("emailfield").value;
+  password = document.getElementById("passwordfield").value;
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle errors here
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error (code " + error.code + "): " + error.message );
+  });
+}
+
 loadTexts();
